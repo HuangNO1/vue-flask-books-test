6e548d048c4eff5762c3ab7d4d6fc249e5a6d9dd
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-05-17 17:02:41 +0800

    flask 給書添加 唯一標識符

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,26 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
+
+'''
+對於更新，我們需要使用唯一識別符號，因為我們不能依靠標題作為唯一。我們可以使用 Python 基本庫 提供的 uuid 作為唯一。
+'''
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
@@ -42,6 +50,7 @@ def all_books():
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
+            'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
@@ -51,6 +60,29 @@ def all_books():
         response_object['books'] = BOOKS
     return jsonify(response_object)
 
+
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    return jsonify(response_object)
+
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
 
 if __name__ == '__main__':
     app.run()

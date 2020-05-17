eccbac056e4c26c654ea8b4ed29682bdf5a2099d
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-05-17 17:14:30 +0800

    添加 update , delete

--- vue-client/src/components/Books.vue
+++ vue-client/src/components/Books.vue
@@ -5,7 +5,7 @@
         <h1>Books</h1>
         <hr />
         <br /><br />
-        <alert :message="message"  v-if="showMessage"></alert>
+        <alert :message="message" v-if="showMessage"></alert>
         <button
           type="button"
           class="btn btn-success btn-sm"
@@ -32,7 +32,12 @@
                 <span v-else>No</span>
               </td>
               <td>
-                <button type="button" class="btn btn-warning btn-sm">
+                <button
+                  type="button"
+                  class="btn btn-warning btn-sm"
+                  v-b-modal.book-update-modal
+                  @click="editBook(book)"
+                >
                   Update
                 </button>
                 <button type="button" class="btn btn-danger btn-sm">
@@ -88,6 +93,50 @@
         <b-button type="reset" variant="danger">Reset</b-button>
       </b-form>
     </b-modal>
+    <b-modal
+      ref="editBookModal"
+      id="book-update-modal"
+      title="Update"
+      hide-footer
+    >
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+        <b-form-group
+          id="form-title-edit-group"
+          label="Title:"
+          label-for="form-title-edit-input"
+        >
+          <b-form-input
+            id="form-title-edit-input"
+            type="text"
+            v-model="editForm.title"
+            required
+            placeholder="Enter title"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-author-edit-group"
+          label="Author:"
+          label-for="form-author-edit-input"
+        >
+          <b-form-input
+            id="form-author-edit-input"
+            type="text"
+            v-model="editForm.author"
+            required
+            placeholder="Enter author"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-read-edit-group">
+          <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Update</b-button>
+        <b-button type="reset" variant="danger">Cancel</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 <script>
@@ -106,7 +155,13 @@ export default {
         author: "",
         read: [],
       },
-      message: '',
+      editForm: {
+        id: "",
+        title: "",
+        author: "",
+        read: [],
+      },
+      message: "",
       showMessage: false,
     };
   },
@@ -129,8 +184,8 @@ export default {
         .post(path, payload)
         .then(() => {
           this.getBooks();
-          this.message = 'Book added!';
-           this.showMessage = true;
+          this.message = "Book added!";
+          this.showMessage = true;
         })
         .catch((error) => {
           // eslint-disable-next-line
@@ -160,6 +215,51 @@ export default {
       evt.preventDefault();
       this.$refs.addBookModal.hide();
       this.initForm();
+    },
+    editBook(book) {
+      this.editForm = book;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      let read = false;
+      if (this.editForm.read[0]) read = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:5000/books/${bookID}`;
+      axios
+        .put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = "Book updated!";
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks(); // why?
+    },
+    initForm() {
+      this.addBookForm.title = "";
+      this.addBookForm.author = "";
+      this.addBookForm.read = [];
+      this.editForm.id = "";
+      this.editForm.title = "";
+      this.editForm.author = "";
+      this.editForm.read = [];
     },
   },
   created() {
